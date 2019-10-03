84ead9b414d5f089f5393e916b8ffdaca50b8aa8
Dean Thomas <dthomas@pixalytics.com>
2019-10-03 16:46:27 +0100

    Revert to manually listing packages.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(name='pixutils',
-      version='0.1.3',
+      version='0.1.4',
       description='Pixalytics python utilities package',
       url='http://github.com/pixalytics-ltd/pixutils',
       author='Pixalytics Ltd',
       author_email='enquiries@pixalytics.com',
       license='MIT',
-      #packages=['pixutils'],
-      packages=find_packages(),
-      namespace_packages=['pixutils'],
+      packages=['pixutils', 'pixutils.gdl'],
+      #packages=find_packages(),
+      #namespace_packages=['pixutils'],
       zip_safe=False)
