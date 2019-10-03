7d7064cb9a59e3afa11f84de5f734515b93eb872
Dean Thomas <dthomas@pixalytics.com>
2019-10-03 16:39:09 +0100

    Use 'find packages' to iterate over sub-packages.

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='pixutils',
-      version='0.1.1',
+      version='0.1.2',
       description='Pixalytics python utilities package',
       url='http://github.com/pixalytics-ltd/pixutils',
       author='Pixalytics Ltd',
       author_email='enquiries@pixalytics.com',
       license='MIT',
-      packages=['pixutils'],
+      #packages=['pixutils'],
+      packages=find_packages(),
       zip_safe=False)
