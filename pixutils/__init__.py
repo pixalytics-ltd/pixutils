8d3c976ccc9ffd4dc025922de642134d3fa1df09
Dean Thomas <dthomas@pixalytics.com>
2020-04-02 10:02:39 +0100

    #7 Added ERA5 download functions to __init__.py.

--- pixutils/__init__.py
+++ pixutils/__init__.py
@@ -1,3 +1,7 @@
 from pixutils.ceres_download import *
+from pixutils.era_download import *
 
-all = ["download_ceres_netflux"]
+
+__all__ = [
+    "download_ceres_netflux",
+    "download_era5_reanalysis_data", "Var"]
