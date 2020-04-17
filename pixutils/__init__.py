d56cda0c6556629fe2407b03a1dd118f43ee4747
Dean Thomas <dthomas@pixalytics.com>
2020-04-17 11:00:35 +0100

    #11 Revert to flat import file for simplicity.

--- pixutils/__init__.py
+++ pixutils/__init__.py
@@ -1,10 +1,15 @@
 from pixutils.ceres_download import *
 from pixutils.era_download import *
-from pixutils.raster_operations import raster_operations
+from pixutils.raster_operations import *
+
+#   todo: split individual imports into better structured sub-modules.
 
 __all__ = [
-    "raster_operations",
     "download_ceres_netflux",
     "download_era5_reanalysis_data",
-    "Var"
+    "Var",
+    #   raster operations
+    "apply_mask",
+    "compress_geotiff",
+    "clamp_raster",
 ]
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pixutils',
-      version='0.1.7',
+      version='0.1.8',
       description='Pixalytics python utilities package',
       url='http://github.com/pixalytics-ltd/pixutils',
       author='Pixalytics Ltd',
